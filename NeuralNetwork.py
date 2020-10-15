f3718e610d0475b0aaa4564fe55d3d926bbe69dd
pietronw <45654238+pietronw@users.noreply.github.com>
2020-10-15 02:06:27 +0200

    Back propagation error and updating weights & biases. Not tested.

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -7,8 +7,9 @@ class NeuralNetwork:
     Layers = list()
     Weights = list()
     Biases = list()
+    Errors = list()
 
-    def __init__(self, n:int, sizes):
+    def __init__(self, n, sizes):
 
         for i in range(len(sizes)-1):
             weights = np.random.rand(sizes[i+1], sizes[i])
@@ -17,7 +18,7 @@ class NeuralNetwork:
             bias = np.random.rand(sizes[i+1])
             self.Biases.append(bias)
 
-    def forward_propagation(self, input: np.array, activate):
+    def forward_propagation(self, input, activate):
         self.Layers.append(input)
 
         for i in range(len(self.Weights)):
@@ -29,9 +30,41 @@ class NeuralNetwork:
 
         return self.Layers[-1]
 
+    def back_propagation_error(self, expected, derivative, C):
+        self.Errors.append(C(expected, self.Layers[-1]))
+
+        for i in reversed(range(1, len(self.Layers)-1)):
+            errors = list()
+            for j in range(len(self.Layers[i])):
+                error = 0
+                for k in range(len(self.Layers[i+1])):
+                    error += self.Weights[i][k][j] * self.Errors[-1][k]
+                errors.append(error)
+            for j in range(len(errors)):
+                errors[j] *= derivative(self.Layers[i][j])
+            self.Errors.append(errors)
+
+        self.Errors.reverse()
+
+    def update_weights(self, factor):
+        for i in range(len(self.Weights)):
+            for j in range(len(self.Layers[i+1])):
+                for k in range(len(self.Layers[i])):
+                    self.Weights[i][j][k] += factor * self.Errors[i][j] * self.Layers[i][k]
+                self.Biases[i][j] += factor * self.Errors[i][j]
+        self.Errors.clear()
+
     @staticmethod
     def sigmoid(x):
         return math.exp(x)/(math.exp(x) + 1)
+
+    @staticmethod
+    def sigmoid_derivative(y):
+        return y(1-y)
+
+    @staticmethod
+    def subtract_cost_function(expected, result):
+        return np.subtract(expected, result)
 
 
 if __name__ == "__main__":
