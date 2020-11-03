75294c07dedc100b2bbacca13a6c5cc1d2dae3fa
pietronw <45654238+pietronw@users.noreply.github.com>
2020-11-03 19:24:56 +0100

    Tests

--- Prediction.py
+++ Prediction.py
@@ -114,31 +114,31 @@ if __name__ == "__main__":
     # wybieramy rozmiar danych - wybierane są odpowiednie pliki
     # oraz przydaje się do odpowiedniego ustawienia danych przy klasyfikacji do więcej niż 2 klas
     samples_per_class = 1000
-    train_data_filename = "regression\\data.activation.train."+str(samples_per_class)+".csv"
-    test_data_filename = "regression\\data.activation.test."+str(samples_per_class)+".csv"
+    train_data_filename = "regression\\data.cube.train."+str(samples_per_class)+".csv"
+    test_data_filename = "regression\\data.cube.test."+str(samples_per_class)+".csv"
     have_to_mix = False
 
     problem = 2  # zgodnie z modes (patrz powyżej)
     n_class = 3  # liczba klas w przypadku problemu klasyfikacji
 
-    iterations = 2000  # iteracje uczenia
-    learning_factor = 0.000001  # współczynnik uczenia
+    iterations = 50000  # iteracje uczenia
+    learning_factor = 0.01  # współczynnik uczenia
     input_size = 1
     output_size = n_class if problem == 1 else 1
 
     multi_class_fl = (problem == 1) & (output_size > 1)
 
-    network = NeuralNetwork([input_size, 10, output_size])
+    network = NeuralNetwork([input_size, 2, 4, 4, output_size])
     in_scaler = Scaler()
     out_scaler = Scaler()
 
-    activation = Activation.tanH
-    derivative = Activation.tanH_derivative
+    activation = Activation.sigmoid
+    derivative = Activation.sigmoid_derivative
     out_activation = Activation.linear
     out_derivative = Activation.linear_derivative
     cost = CostFunctions.MSE
-    cost_gradient = CostFunctions.kullback_gradient
-    in_scaling = 3
+    cost_gradient = CostFunctions.MSE_gradient
+    in_scaling = 1
     out_scaling = 1
 
     #Visualization.write_out_neural_network_params(network)
